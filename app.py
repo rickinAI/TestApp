db54b3f3af9a110e1b3b1036eb73b274fea14619
rickinAI <136267670+rickinAI@users.noreply.github.com>
2023-06-13 23:44:18 -0400

    Updated app with code

--- app.py
+++ app.py
@@ -1,11 +1,114 @@
-#-----------------------------------------------------------------------------------------
-# Copyright (c) Microsoft Corporation. All rights reserved.
-# Licensed under the MIT License. See LICENSE in the project root for license information.
-#-----------------------------------------------------------------------------------------
-
 from flask import Flask
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return app.send_static_file("index.html")
+import os
+
+import chainlit as cl
+from dotenv import load_dotenv
+from langchain import PromptTemplate
+from langchain.agents import AgentType, Tool, initialize_agent
+from langchain.chains import ConversationChain
+from langchain.chains.conversation.memory import ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
+from langchain.tools import DuckDuckGoSearchRun
+from langchain.utilities import WikipediaAPIWrapper
+
+load_dotenv("gpt.env")
+
+# OpenAI API key
+OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
+
+llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo")
+
+search = DuckDuckGoSearchRun()
+wikipedia = WikipediaAPIWrapper()
+
+# Web Search Tool
+search_tool = Tool(
+    name="Web Search",
+    func=search.run,
+    description="A useful tool for searching the Internet to find information on world events, issues, etc. Worth using for general topics. Use precise questions.",
+)
+
+# Wikipedia Tool
+wikipedia_tool = Tool(
+    name="Wikipedia",
+    func=wikipedia.run,
+    description="A useful tool for searching the Internet to find information on world events, issues, etc. Worth using for general topics. Use precise questions.",
+)
+
+prompt = PromptTemplate(
+    template="""Plan: {input}
+
+History: {chat_history}
+
+Let's think about answer step by step.
+If it's information retrieval task, solve it like a professor in particular field.""",
+    input_variables=["input", "chat_history"],
+)
+
+plan_prompt = PromptTemplate(
+    input_variables=["input", "chat_history"],
+    template="""Prepare plan for task execution. (e.g. retrieve current date to find weather forecast)
+
+    Tools to use: wikipedia, web search
+
+    REMEMBER: Keep in mind that you don't have information about current date, temperature, informations after September 2021. Because of that you need to use tools to find them.
+
+    Question: {input}
+
+    History: {chat_history}
+
+    Output look like this:
+    '''
+        Question: {input}
+
+        Execution plan: [execution_plan]
+
+        Rest of needed information: [rest_of_needed_information]
+    '''
+
+    IMPORTANT: if there is no question, or plan is not need (YOU HAVE TO DECIDE!), just populate {input} (pass it as a result). Then output should look like this:
+    '''
+        input: {input}
+    '''
+    """,
+)
+
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+
+plan_chain = ConversationChain(
+    llm=llm,
+    memory=memory,
+    input_key="input",
+    prompt=plan_prompt,
+    output_key="output",
+)
+
+# Initialize Agent
+agent = initialize_agent(
+    agent=AgentType.CHAT_CONVERSATIONAL_REACT_DESCRIPTION,
+    tools=[search_tool, wikipedia_tool],
+    llm=llm,
+    verbose=True, # verbose option is for printing logs (only for development)
+    max_iterations=3,
+    prompt=prompt,
+    memory=memory,
+)
+
+@cl.langchain_run
+def run(agent, input_str):
+    # Plan execution
+    plan_result = plan_chain.run(input_str)
+
+    # Agent execution
+    res = agent(plan_result)
+
+    # Send message
+    cl.Message(content=res["output"]).send()
+
+
+@cl.langchain_factory
+def factory():
+    return agent
