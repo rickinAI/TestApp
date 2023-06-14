61cc2096dd6ea80b816d760c76db6e0d7c286ebd
rickinAI <136267670+rickinAI@users.noreply.github.com>
2023-06-13 23:51:59 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 
 import os
-
 import chainlit as cl
 from dotenv import load_dotenv
 from langchain import PromptTemplate
@@ -12,8 +11,6 @@ from langchain.chains.conversation.memory import ConversationBufferMemory
 from langchain.chat_models import ChatOpenAI
 from langchain.tools import DuckDuckGoSearchRun
 from langchain.utilities import WikipediaAPIWrapper
-
-load_dotenv("gpt.env")
 
 # OpenAI API key
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
