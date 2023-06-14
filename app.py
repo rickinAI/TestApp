0ba0557b37b7005195b75f95ddbca9027bc0f9f0
rickinAI <136267670+rickinAI@users.noreply.github.com>
2023-06-14 05:18:40 +0000

    change to code

--- app.py
+++ app.py
@@ -1,6 +1,3 @@
-from flask import Flask
-app = Flask(__name__)
-
 import os
 import chainlit as cl
 from dotenv import load_dotenv
@@ -12,7 +9,13 @@ from langchain.chat_models import ChatOpenAI
 from langchain.tools import DuckDuckGoSearchRun
 from langchain.utilities import WikipediaAPIWrapper
 
+
+# Load environment variables from .env file
 load_dotenv("api.env")
+
+# Initialize the Flask app
+from flask import Flask
+app = Flask(__name__)
 
 # OpenAI API key
 OPENAI_API_KEY = os.environ["OPENAI_API_KEY"]
