5f2c947cc4577859be1e56db0258f5495fc99a9e
rickinAI <136267670+rickinAI@users.noreply.github.com>
2023-06-14 00:41:03 -0400

    Update app.py

--- app.py
+++ app.py
@@ -109,3 +109,8 @@ def run(agent, input_str):
 @cl.langchain_factory
 def factory():
     return agent
+
+# Run the Flask app
+if __name__ == "__main__":
+    logging.info("Flask app started")
+    flask_app.run(host="0.0.0.0", port=8000)
